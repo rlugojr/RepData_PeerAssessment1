d0fcd4a7538f4fe783ba1b51f3dfef96236c8312
Ray Lugo, Jr <rlugojr@gmail.com>
2017-02-11 17:05:57 -0500

    Question 2 answered
    Calculated Mean steps per interval across all days.
    Plot of mean steps per interval in time series.
    Identified interval with max average steps.
    Plotted vertical line on time series for interval with max steps

--- src/activity_analysis.R
+++ src/activity_analysis.R
@@ -14,7 +14,7 @@ activity_data <- readr::read_csv(file = "data/activity.zip", col_names = TRUE, c
 
 
 ##Question 1: What is mean total number of steps taken per day?
-### Make a vector for number of steps (excluding NA.)
+### Make a data.frame for total number of steps per day (excluding NA.)
 total_steps_per_day <- activity_data %>%
     filter(!is.na(steps)) %>%
     select(date, steps) %>%
@@ -22,15 +22,51 @@ total_steps_per_day <- activity_data %>%
     summarise(totalSteps = sum(steps))
 
 ### Plot histogram of steps per day.
-ggplot(data = total_steps_per_day, aes(date, totalSteps, fill = totalSteps)) +
+plot_histogram_total_steps_per_day <- ggplot(data = total_steps_per_day, aes(date, totalSteps, fill = totalSteps)) +
     geom_bar(stat = "identity", color = "steelblue") +
     scale_x_date(date_labels = "%m/%d", date_minor_breaks = "1 day") +
-    labs(title = "Total Steps per Day", x = "Date", y = "Steps")
+    labs(title = "Total Steps per Day", x = "Date", y = "Steps") +
+    ggsave(filename = "figures/histogram_total_steps_per_day.png", width = 5, height = 3)
 
-#calculate the mean value of total steps
+plot_histogram_total_steps_per_day
+
+###calculate the mean value of total steps
 meanTotalSteps <- mean(total_steps_per_day$totalSteps)
-meanTotalSteps
+print(paste0("Mean total steps per day: ", meanTotalSteps))
 
-#calculate the mid value of total steps
+###calculate the mid value of total steps
 midTotalSteps <- median(total_steps_per_day$totalSteps)
-midTotalSteps
+print(paste0("Median total steps per day: ", midTotalSteps))
+
+
+##Question 2: What is the average daily activity pattern?
+### Make a data.frame for mean steps taken per interval across all days
+mean_steps_per_interval_all_days <- activity_data %>%
+    filter(!is.na(steps)) %>%
+    select(interval, steps) %>%
+    group_by(interval) %>%
+    summarise(meanSteps = mean(steps))
+
+### Plot meanSteps by interval in time series plot
+plot_time_series_mean_steps_per_interval <- ggplot(data = mean_steps_per_interval_all_days, aes(interval, meanSteps)) +
+    geom_step(color = 'blue', lwd = 0.25) +
+    scale_x_continuous(breaks = c(seq(0,2500,250)),minor_breaks = c(seq(0,2500,50))) +
+    theme_bw() +
+    labs(title = "Average Steps per Interval", x = "Interval", y = "Average Steps") +
+    ggsave(filename = "figures/time_series_mean_steps_per_interval.png", width = 5, height = 3)
+
+plot_time_series_mean_steps_per_interval
+
+### plot vline at interval with max steps
+interval_max_steps = mean_steps_per_interval_all_days %>%
+    select(interval, meanSteps) %>%
+    filter(meanSteps == max(meanSteps))
+
+print(paste0("Interval with max average steps per day: ", interval_max_steps$interval))
+
+plot_time_series_interval_max_steps = plot_time_series_mean_steps_per_interval +
+    geom_vline(xintercept = interval_max_steps$interval, color = "red", lwd = 0.1, linetype = "longdash") +
+    geom_text(aes(x  = interval_max_steps$interval, y = 0), nudge_x = 200, size = 3, label = paste0("Interval: ",interval_max_steps$interval), color = "red") +
+    ggsave(filename = "figures/time_series_interval_max_steps.png", width = 5, height = 3)
+
+plot_time_series_interval_max_steps
