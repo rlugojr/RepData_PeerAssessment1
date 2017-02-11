c0341d524f887c8d221a9ccd14be045ac07bf8f8
Ray Lugo, Jr <rlugojr@gmail.com>
2017-02-11 11:13:13 -0500

    Reorg folders and files
    Reorganized and renamed folders and files according to the instructions
    included.

--- src/activity_analysis.R
+++ src/activity_analysis.R
@@ -7,7 +7,7 @@
 #prepare environment
 library(tidyverse)
 library(lubridate)
-library(xtables)
+library(xtable)
 
 #load data from zip
 activity_data <- readr::read_csv(file = "data/activity.zip", col_names = TRUE, col_types = readr::cols(readr::col_integer(),readr::col_date(format = "%Y-%m-%d"),readr::col_integer()))
